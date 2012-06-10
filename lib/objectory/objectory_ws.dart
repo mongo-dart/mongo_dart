c76438178936eadd5c75014f2b2fa8fee13d1c95
Vadim Tsushko <vadimtsushko@gmail.com>
2012-06-10 16:31:06 +0600

    files renaming

--- lib/objectory/objectory_ws.dart
+++ lib/objectory/objectory_ws.dart
@@ -2,7 +2,7 @@
 #import("dart:io");
 #import("../bson/bson.dart");
 #source("persistent_object.dart");
-#source("objectory.dart");
+#source("objectory_base.dart");
 #source("schema.dart");
 #source("persistent_list.dart");
 #source("objectory_query_builder.dart");
