2dedb4bcfbae5e131ffd2018b517587c07acc92c
Vadim Tsushko <vadimtsushko@gmail.com>
2012-06-11 13:00:34 +0600

    log4dart to submodule

--- third_party/log4dart/Appender.dart
+++
@@ -1,9 +0,0 @@
-/**
- * Implemented by classes that performs the actual logging
- */
-interface Appender {
-  /**
-   * Log a message. The message can be any type, this is done to facilitate a wide range of logging mecahnicms.
-   */
-  void doAppend(Dynamic logMessage);
-}
--- third_party/log4dart/ConsoleAppender.dart
+++
@@ -1,8 +0,0 @@
-/**
- * Appender that logs to the console
- */
-class ConsoleAppender implements Appender {
-  void doAppend(String message) {
-    print(message);      
-  }
-}
