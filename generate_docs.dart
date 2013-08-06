cbb672b958b6d8e3b67994e4995814a86aaf8fa1
Vadim Tsushko <vadimtsushko@gmail.com>
2013-08-06 16:45:32 +0600

    remove file

--- generate_docs.dart
+++
@@ -1,7 +0,0 @@
-import 'package:ghpages_generator/ghpages_generator.dart' as gh;
-
-main() {
-  new gh.Generator()
-  ..setDartDoc(['lib/mongo_dart.dart'], excludedLibs: ['path'])
-  ..generate();
-}
