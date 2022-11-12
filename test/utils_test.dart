b158d2d57a5cdc0a8b66eba33b5d4a45cf0e7984
Giorgio Franceschetti <giorgiofran@gmail.com>
2022-11-12 12:18:06 +0100

    dev 8 bis

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -1,9 +1,11 @@
+import 'package:basic_utils/basic_utils.dart';
 import 'package:mongo_dart/src/utils/check_same_domain.dart'
     show checkSameDomain;
+import 'package:mongo_dart/src/utils/decode_dns_seed_list.dart';
 import 'package:mongo_dart/src/utils/split_hosts.dart';
 import 'package:test/test.dart' show expect, group, isFalse, isTrue, test;
 
-import '../test_old/utils/throw_utils.dart' show throwsMongoDartError;
+import 'utils/matcher/mongo_dart_error_matcher.dart';
 
 void main() {
   group('Check Same Domain', () {
@@ -121,6 +123,99 @@ void main() {
               'cluster0-shard-00-02-smeth.gcp.mongodb.net:27017/'
               'test?authSource=admin&ssl=true'),
           throwsMongoDartError);
+    });
+  });
+
+  group('Decode Dns Seed List', () {
+    test('Testing connection TXT', () async {
+      var records =
+          await DnsUtils.lookupRecord('rs.joedrumgoole.com', RRecordType.TXT);
+      expect(records?.first.data, 'authSource=admin&replicaSet=srvdemo');
+    });
+    test('Testing connection SRV', () async {
+      var records = await DnsUtils.lookupRecord(
+          '_mongodb._tcp.' 'rs.joedrumgoole.com', RRecordType.SRV);
+
+      expect(records?.first.data, '0 0 27022 rs1.joedrumgoole.com.');
+      expect(records?[1].data, '0 0 27022 rs2.joedrumgoole.com.');
+      expect(records?.last.data, '0 0 27022 rs3.joedrumgoole.com.');
+    });
+
+    test('Decode Dns Seedlist', () async {
+      var result =
+          await decodeDnsSeedlist(Uri.parse('mongodb+srv://user:password@'
+              'rs.joedrumgoole.com/test?retryWrites=true&w=majority'));
+
+      expect(
+          result.first,
+          'mongodb://user:password@rs1.joedrumgoole.com:27022/'
+          'test?authSource=admin&replicaSet=srvdemo&'
+          'retryWrites=true&w=majority&ssl=true');
+      expect(
+          result[1],
+          'mongodb://user:password@rs2.joedrumgoole.com:27022/'
+          'test?authSource=admin&replicaSet=srvdemo&'
+          'retryWrites=true&w=majority&ssl=true');
+      expect(
+          result.last,
+          'mongodb://user:password@rs3.joedrumgoole.com:27022/'
+          'test?authSource=admin&replicaSet=srvdemo&'
+          'retryWrites=true&w=majority&ssl=true');
+    });
+    test('Decode Dns Seedlist - sync', () {
+      decodeDnsSeedlist(Uri.parse('mongodb+srv://user:password@'
+              'rs.joedrumgoole.com/test?retryWrites=true&w=majority'))
+          .then((result) {
+        expect(
+            result.first,
+            'mongodb://user:password@rs1.joedrumgoole.com:27022/'
+            'test?authSource=admin&replicaSet=srvdemo&'
+            'retryWrites=true&w=majority&ssl=true');
+        expect(
+            result[1],
+            'mongodb://user:password@rs2.joedrumgoole.com:27022/'
+            'test?authSource=admin&replicaSet=srvdemo&'
+            'retryWrites=true&w=majority&ssl=true');
+        expect(
+            result.last,
+            'mongodb://user:password@rs3.joedrumgoole.com:27022/'
+            'test?authSource=admin&replicaSet=srvdemo&'
+            'retryWrites=true&w=majority&ssl=true');
+      });
+    });
+    test('Decode Dns Seedlist - Wrong host error', () async {
+      expect(
+          () async => decodeDnsSeedlist(Uri.parse('mongodb+srv://user:password@'
+              'rsx.joedrumgoole.com/test?retryWrites=true&w=majority')),
+          throwsMongoDartError);
+    });
+    test('Decode Dns Seedlist - More than one host error', () async {
+      expect(
+          () async => decodeDnsSeedlist(Uri.parse('mongodb+srv://user:password@'
+              'rs.joedrumgoole.com, rs2.joedrumgoole.com/'
+              'test?retryWrites=true&w=majority')),
+          throwsMongoDartError);
+    });
+    test('Db creation with seedlist format url', () async {
+      var uriList =
+          await decodeDnsSeedlist(Uri.parse('mongodb+srv://user:password@'
+              'rs.joedrumgoole.com/test?retryWrites=true&w=majority'));
+      var urilist = uriList;
+      expect(
+          urilist.first,
+          'mongodb://user:password@rs1.joedrumgoole.com:27022/'
+          'test?authSource=admin&replicaSet=srvdemo&'
+          'retryWrites=true&w=majority&ssl=true');
+      expect(
+          urilist[1],
+          'mongodb://user:password@rs2.joedrumgoole.com:27022/'
+          'test?authSource=admin&replicaSet=srvdemo&'
+          'retryWrites=true&w=majority&ssl=true');
+      expect(
+          urilist.last,
+          'mongodb://user:password@rs3.joedrumgoole.com:27022/'
+          'test?authSource=admin&replicaSet=srvdemo&'
+          'retryWrites=true&w=majority&ssl=true');
     });
   });
 }
